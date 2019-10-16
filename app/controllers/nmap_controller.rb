85d733aad5bac643eeb372312bd1e8385a388afd
Kyle Sullivan <ksullivan@roundsphere.com>
2019-10-16 13:13:34 -0400

    Remove unused controller

--- app/controllers/nmap_controller.rb
+++
@@ -1,5 +0,0 @@
-class NmapController < ApplicationController
-  def index
-    render json: { controller: "nmap" }
-  end
-end
--- test/controllers/nmap_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class NmapControllerTest < ActionDispatch::IntegrationTest
-  test "should get import" do
-    get nmap_import_url
-    assert_response :success
-  end
-
-end
